56caf4f629d55897a983d3ad7c187f1867ecbaf5
Gabriele <gabrieleingrassia3@gmail.com>
2023-06-26 18:23:43 +0200

    ex 2.3

---
+++ initial/src/main/java/com/example/springboot/MealConDescrizione.java
@@ -0,0 +1,37 @@
+package com.example.springboot;
+
+public class MealConDescrizione {
+    private String name;
+    private Double price;
+    private String description;
+    public MealConDescrizione(String name, Double price, String description){
+        this.name=name;
+        this.price=price;
+        this.description=description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
+
--- initial/src/main/java/com/example/springboot/MealController.java
+++ initial/src/main/java/com/example/springboot/MealController.java
@@ -12,29 +12,47 @@ import java.util.stream.Collectors;
 
 @RestController
 public class MealController {
-    private List<Meal> mealList = new ArrayList<>(List.of(
-            new Meal("Pasta al forno", 4.90),
-            new Meal("Carbonara", 10.90),
-            new Meal("Pennette primavera", 7.50),
-            new Meal("Gnocchi di patate", 5.90),
-            new Meal("Spaghetti col sugo", 3.90),
-            new Meal("Linguine ai frutti di mare", 11.90),
-            new Meal("Farfallette al salmone", 9.90)
-    ));
-
+    private List<Meal> mealList = new ArrayList<>(
+            List.of(
+                    new Meal("Pasta al forno", 4.90),
+                    new Meal("Carbonara", 10.90),
+                    new Meal("Pennette primavera", 7.50),
+                    new Meal("Gnocchi di patate", 5.90),
+                    new Meal("Spaghetti col sugo", 3.90),
+                    new Meal("Linguine ai frutti di mare", 11.90),
+                    new Meal("Farfallette al salmone", 9.90)
+            )
+    );
+    private List<MealConDescrizione> mealDescriptionList = new ArrayList<>(
+            List.of(
+                    new MealConDescrizione("Pasta al forno", 4.90, "Anelletti con ragù al forno"),
+                    new MealConDescrizione("Carbonara", 10.90, "Spaghetti con guanciale e formaggi"),
+                    new MealConDescrizione("Pennette primavera", 7.50, "Penne con panna, piselli e prosciutto"),
+                    new MealConDescrizione("Gnocchi di patate", 5.90, "Pasta di patate condita con burro"),
+                    new MealConDescrizione("Spaghetti col sugo", 3.90, "Spaghetti con sugo e basilico"),
+                    new MealConDescrizione("Linguine ai frutti di mare", 11.90, "Linguine con cozze, vongole, calamaro e pomodorino"),
+                    new MealConDescrizione("Farfallette al salmone", 9.90, "Farfallette con panna e salmone affmicato e prezzemolo")
+            )
+    );
 //    @GetMapping(value = "/meals")
 //    public ResponseEntity<List<Meal>> response() {
 //        return ResponseEntity.ok(mealList);
 //    }
 
 
-    @GetMapping(value = "/meals/{name}")
-    public ResponseEntity<Meal> getByName(@PathVariable("name") String name) {
-        Meal m = new Meal("", 0.0);
-        List<Meal> theMeal= mealList.stream().filter(n->n.getName().equalsIgnoreCase(name)).toList();
-        m.setName(theMeal.get(0).getName());
-        m.setPrice(theMeal.get(0).getPrice());
-        return ResponseEntity.ok(m);
+//    @GetMapping(value = "/meals/{name}")
+//    public ResponseEntity<Meal> getByName(@PathVariable("name") String name) {
+//        Meal m = new Meal("", 0.0);
+//        List<Meal> theMeal= mealList.stream().filter(n->n.getName().equalsIgnoreCase(name)).toList();
+//        m.setName(theMeal.get(0).getName());
+//        m.setPrice(theMeal.get(0).getPrice());
+//        return ResponseEntity.ok(m);
+//    }
+
+    @GetMapping(value = "/meal/{phrase}")
+    public ResponseEntity<List<MealConDescrizione>> response(@PathVariable("phrase") String text) {
+        List<MealConDescrizione> mList= mealDescriptionList.stream().filter(n->n.getDescription().contains(text)).toList();
+        return ResponseEntity.ok(mList);
     }
 
 }
